f5b2342b1f1dcabcf2261b62efdb980a59d2dc2b
Anita <anamaridaviladejesus@gmail.com>
2026-03-25 10:56:11 -0400

    idk

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ import flet as ft
 import requests 
 
 def main(page: ft.Page):
-    pass
+
+    page.window.full_screen = True
+    #page.theme = ft.Theme(font_family="Main")
+    page.title= "World Of Cinema"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    page.bgcolor = "#ffffff"
+    page.expand=True
+    page.update()
+
+    MainStack = ft.Stack(
+        width=1800, 
+        height=750,
+        controls=[ft.Image(
+            src="BG.png",
+            width=1800,
+            height=750)])
+    
+    page.add(MainStack)
+    page.update()
 
 ft.run(main, assets_dir="assets")    
