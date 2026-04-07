020305ad4f13fd6dec55ea19194bf2c535f64a9f
Anita <anamaridaviladejesus@gmail.com>
2026-04-07 12:48:32 -0400

    I added stuff idk

--- main.py
+++ main.py
@@ -17,15 +17,21 @@ def main(page: ft.Page):
     page.expand=True
     page.update()
 
-    
+    #Background Image
     Background= ft.Image(
                 src="lBG.png", fit=ft.Image.fit, scale= 1.2)
+    
+    #Ball For Backgroun
     Ball = ft.Image(src="Ball.png", top=478.6, left=-730.9, expand=False)
+
+    #Main Text
+    TitlePT1 = ft.Text(value = "WORLD OF",color= '#d5b586', size=84, left = 194)
+    TittlePT2 = ft.Text(value= "CINEMA", color = ft.Colors.WHITE, size = 300)
 
 
     MainStack = ft.Stack(
         expand=True,
-        controls=[Background,Ball])
+        controls=[Background,Ball, TitlePT1, TittlePT2])
     
     
     
