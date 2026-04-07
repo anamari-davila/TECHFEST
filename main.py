3e62cb42e9ae53674bf7961bd91fb6831051b909
Franklin <rosariosanchezfranklin@gmail.com>
2026-04-07 10:45:56 -0400

    Add

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ import requests
 
 def main(page: ft.Page):
 
-    page.window.full_screen = True
+    page.window.full_screen = False
     #page.theme = ft.Theme(font_family="Main")
     page.title= "World Of Cinema"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    page.bgcolor = "#ffffff"
+    page.bgcolor = "#000000"
     page.expand=True
     page.update()
 
+
+    
+
     MainStack = ft.Stack(
-        width=1800, 
-        height=750,
+        width=1920, 
+        height=1080,
         controls=[ft.Image(
-            src="BG.png",
-            width=1800,
-            height=750)])
+                src="lBG.jpg")])
+    
+    
     
     page.add(MainStack)
     page.update()
