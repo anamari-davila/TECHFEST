3d87c83352481032ad6a0004e69c5fd32e357d4e
Franklin <rosariosanchezfranklin@gmail.com>
2026-04-14 02:16:09 -0400

    PopCorn Image and title
    Yoooooooooooooooooooooooooo

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import asyncio
 
 def main(page: ft.Page):
 
+    
+    
     #fonts
     page.fonts = {
-        "Main": "Comucan_PERSONAL_USE_ONLY.otf"
+        "Main": "Comucan_PERSONAL_USE_ONLY.otf",
+
+        "YellowMain": "TtNormsExtraBlack.otf",
+        "Whitemain": "BricolageBoldened.ttf"
     }
+
+    #Page Setup
 
     page.window.full_screen = False
     # page.theme = ft.Theme(font_family="Main")
@@ -17,6 +24,8 @@ def main(page: ft.Page):
     page.bgcolor = "#000000"
     page.expand=True
     page.update()
+
+    #Balls functions
 
     async def bola1():
 
@@ -64,18 +73,61 @@ def main(page: ft.Page):
         Ball.update()
 
 
+    # Background and ball
     Background= ft.Image(
-                src="lBG.png", fit=ft.Image.fit, scale= 1.2)
-    Ball = ft.Image(src="Ball.png", top=390.6, left=-630.9, animate_opacity=300)
+                src="lBG.png", 
+                fit=ft.Image.fit, 
+                scale= 1.2
+                )
+    
+    Ball = ft.Image(
+        src="Ball.png", 
+        top=390.6, 
+        left=-630.9, animate_opacity=300
+        )
+
+    #MainPageTextFields
+    MainYellow= ft.Text(
+        value="WORLD OF",
+        font_family="YellowMain", 
+        color="#d5b586", 
+        top=100, 
+        left=315, 
+        size=87.5,
+        style=ft.TextStyle(letter_spacing=0)
+        )
+    
+    MainWhite= ft.Text(
+        value= "CINEMA", 
+        font_family= "Whitemain", 
+        top= 80, 
+        left= 40, 
+        size=376,
+        style=ft.TextStyle(letter_spacing=-5)
+        )
+    
+    #Popcorn image and icons
+
+    PopCorn = ft.Image(
+        src="PopCorn.png",
+        top=165,
+        left=-545,
+        expand=False,
+        scale=0.72,
+        )
+
+    # l1= ft.Button(content="1st", on_click=bola1, top=100)
+    # l2= ft.Button(content="2nd", on_click=bola2,top=200)
+    # l3= ft.Button(content="3rd", on_click=bola3, top= 300)
 
 
-    l1= ft.Button(content="1st", on_click=bola1, top=100)
-    l2= ft.Button(content="2nd", on_click=bola2,top=200)
-    l3= ft.Button(content="3rd", on_click=bola3, top= 300)
+
+    #Main Page
 
     MainStack = ft.Stack(
         expand=True,
-        controls=[Background,Ball, l1, l2, l3])
+        controls=[Background, Ball, MainYellow, MainWhite, PopCorn]
+        )
     
     
     
