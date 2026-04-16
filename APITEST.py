a5608117a4385223a35710645a86c61163cb5e21
Franklin <rosariosanchezfranklin@gmail.com>
2026-04-16 14:45:16 -0400

    Testing APIS
    Blah

--- APITEST.py
+++ APITEST.py
@@ -1,4 +1,28 @@
 import requests
 import os
+from dotenv import load_dotenv
 
-API_KEY= os.
+
+load_dotenv()
+API_KEY= os.getenv("TMDB_API_KEY")
+
+
+if not API_KEY:
+    raise ValueError("TMDB_API_KEY not found")
+else:
+    print("Successful")
+
+
+url = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {API_KEY}"
+    
+}
+
+response = requests.get(url, headers=headers)
+
+data = response.json()
+
+print(data["results"][0]["title"]) #1st movie
