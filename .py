b067848d3c27c3f3a6888820e5a9a4762b54383b
thaicebayard <thaicebayard@gmail.com>
2026-04-28 11:59:04 -0400

    DB testing

---
+++ .py
@@ -0,0 +1,35 @@
+import sqlite3
+
+conn = sqlite3.connect("crepe_stand.db")
+
+cursor = conn.cursor()
+createCrepeTablequery = """
+                        CREATE TABLE IF NOT EXISTS Movies(
+                            idMovie INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                            Hour VARCHAR(8)
+                        );
+
+                        CREATE TABLE IF NOT EXISTS Reservations(
+                            idReservation INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                            idMovie INTEGER NOT NULL,
+                            idSeats INTEGER NOT NULL,
+                            Hour VARCHAR(8),
+                            FOREIGN KEY (idMovie) REFERENCES Movies(idMovie),
+                            FOREIGN KEY (idSeats) REFERENCES Seats(idSeats)
+                        );
+
+                        CREATE TABLE IF NOT EXISTS Seats(
+                            idSeats INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                            Name VARCHAR(3)
+                        );
+
+                        CREATE TABLE IF NOT EXISTS Room(
+                            idRoom INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                            Room VARCHAR(2),
+                            idMovie INTEGER NOT NULL,
+                            Seat INTEGER NOT NULL,
+                            FOREIGN KEY (idMovie) REFERENCES Movies(idMovie),
+                            FOREIGN KEY (Seat) REFERENCES Seats(idSeats)
+                        );"""
+
+cursor.execute(createCrepeTablequery)
